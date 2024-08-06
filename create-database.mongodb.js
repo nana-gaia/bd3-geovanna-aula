66807661b5b2e9596540f3a86a730db37377a8eb
nana-gaia <geovannacarol.2003@gmail.com>
2024-08-06 20:43:51 -0300

    CRIANDO O BANCO DE DADOS

---
+++ create-database.mongodb.js
@@ -0,0 +1,11 @@
+/* NOME DO BANCO DE DADOS */
+const database ='BD3-GEOVANNA-AULA';
+
+/* NOME DO BANCO DA COEÇÃO DE DADOS */
+const collection ='LIVRARIA';
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS */
+use(database);
+
+/* CRIAR UMA COLEÇÃO DE DADOS */
+db.createCollection(collection);
