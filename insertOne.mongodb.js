ddc20577f7f6a0e72b34617e6645671ee6289987
nana-gaia <geovannacarol.2003@gmail.com>
2024-08-20 20:26:38 -0300

    INSERTÇÃO DE SELEÇÃO DE DADOS

---
+++ insertOne.mongodb.js
@@ -0,0 +1,22 @@
+/* NOME DO BANCO DE DADOS */
+const database ='BD3-GEOVANNA-AULA';
+
+const collection = 'Livraria';
+
+use(database);
+
+db[collection].insertOne(
+
+[
+    {
+        "codigo":"3",
+        "titulo": "Os Robôs da Alvorada",
+        "autor": "Isaac Asimov",
+        "descricao": "Em meio à mais evoluída colônia humana fora da Terra, ocorre um crime sem precedentes: um robô humaniforme é assassinado, colocando em xeque a reputação de um importante estudioso daquele planeta, único suspeito do crime.",
+        "imagem":"/livros/robos_alvorada.jpg",
+        "valor": 115,
+        "categoria":"Ficção Científica",
+        "teste":"teste 123"
+      },
+    ]
+);
